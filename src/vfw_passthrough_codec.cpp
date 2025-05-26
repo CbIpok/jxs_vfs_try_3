831a59e79f408c2ff0006b42ffa77f816ee7fb9a
cypok is not CbIpok <dmitrienkomy@gmail.com>
2025-05-27 06:14:15 +0700

    wohooo!

--- src/vfw_passthrough_codec.cpp
+++ src/vfw_passthrough_codec.cpp
@@ -16,7 +16,7 @@ extern "C" {
 #endif
 
 // Standard exports
-STDAPI CALLBACK DriverProc(
+LRESULT CALLBACK DriverProc(
     DWORD_PTR dwDriverID,
     HDRVR hDriver,
     UINT uMsg,
@@ -32,14 +32,16 @@ STDAPI DllUnregisterServer();
 
 // Sequence-compression exports to satisfy testEncoder
 // Note: use LPBITMAPINFO here (not LPBITMAPINFOHEADER)
-typedef BOOL    (WINAPI *SeqStartFunc)(PCOMPVARS, LPBITMAPINFO);
-typedef LPVOID  (WINAPI *SeqFrameFunc)(PCOMPVARS, UINT, LPVOID, BOOL*, LONG*);
-typedef void    (WINAPI *SeqEndFunc)(PCOMPVARS);
+typedef BOOL (WINAPI *SeqStartFunc)(PCOMPVARS, LPBITMAPINFO);
 
-static HMODULE      g_hVfw32    = nullptr;
+typedef LPVOID (WINAPI *SeqFrameFunc)(PCOMPVARS, UINT, LPVOID, BOOL *, LONG *);
+
+typedef void (WINAPI *SeqEndFunc)(PCOMPVARS);
+
+static HMODULE g_hVfw32 = nullptr;
 static SeqStartFunc g_pSeqStart = nullptr;
 static SeqFrameFunc g_pSeqFrame = nullptr;
-static SeqEndFunc   g_pSeqEnd   = nullptr;
+static SeqEndFunc g_pSeqEnd = nullptr;
 
 static void InitSeqFuncs() {
     if (!g_hVfw32) {
@@ -47,7 +49,7 @@ static void InitSeqFuncs() {
         if (g_hVfw32) {
             g_pSeqStart = (SeqStartFunc) GetProcAddress(g_hVfw32, "ICSeqCompressFrameStart");
             g_pSeqFrame = (SeqFrameFunc) GetProcAddress(g_hVfw32, "ICSeqCompressFrame");
-            g_pSeqEnd   = (SeqEndFunc)   GetProcAddress(g_hVfw32, "ICSeqCompressFrameEnd");
+            g_pSeqEnd = (SeqEndFunc) GetProcAddress(g_hVfw32, "ICSeqCompressFrameEnd");
         }
     }
 }
@@ -62,7 +64,7 @@ struct PassThroughCodec {
     BITMAPINFOHEADER outputHeader;
 };
 
-static const DWORD_PTR INVALID_DRIVER_ID = (DWORD_PTR)-1;
+static const DWORD_PTR INVALID_DRIVER_ID = (DWORD_PTR) -1;
 using DefDriverProcPtr = LRESULT (WINAPI*)(DWORD_PTR, HDRVR, UINT, LPARAM, LPARAM);
 static DefDriverProcPtr g_pDefDriverProc = nullptr;
 
@@ -84,12 +86,12 @@ BOOL WINAPI DllMain(HINSTANCE, DWORD, LPVOID) {
     return TRUE;
 }
 
-STDAPI CALLBACK DriverProc(
+LRESULT CALLBACK DriverProc(
     DWORD_PTR dwDriverID,
-    HDRVR hDriver,
-    UINT uMsg,
-    LPARAM lParam1,
-    LPARAM lParam2
+    HDRVR     hDriver,
+    UINT      uMsg,
+    LPARAM    lParam1,
+    LPARAM    lParam2
 ) {
     LRESULT res = ICERR_UNSUPPORTED;
     auto *pCodec = reinterpret_cast<PassThroughCodec *>(dwDriverID);
@@ -108,12 +110,15 @@ STDAPI CALLBACK DriverProc(
                 res = INVALID_DRIVER_ID;
             } else {
                 pCodec = new(std::nothrow) PassThroughCodec();
-                res = pCodec ? reinterpret_cast<LRESULT>(pCodec) : DRV_CANCEL;
+                res = pCodec ? reinterpret_cast<LRESULT>(pCodec)
+                             : DRV_CANCEL;
             }
             break;
 
         case DRV_CLOSE:
-            if (pCodec) delete pCodec;
+            if (pCodec) {
+                delete pCodec;
+            }
             res = DRV_OK;
             break;
 
@@ -124,23 +129,30 @@ STDAPI CALLBACK DriverProc(
                 switch (uMsg) {
                     case ICM_GETINFO: {
                         auto *pci = reinterpret_cast<ICINFO *>(lParam1);
-                        if (!pci || lParam2 < static_cast<LPARAM>(sizeof(ICINFO))) {
+                        if (!pci ||
+                            lParam2 < static_cast<LPARAM>(sizeof(ICINFO)))
+                        {
                             res = ICERR_BADPARAM;
                         } else {
-                            pci->dwSize = sizeof(ICINFO);
-                            pci->fccType = ICTYPE_VIDEO;
+                            pci->dwSize    = sizeof(ICINFO);
+                            pci->fccType   = ICTYPE_VIDEO;
                             pci->fccHandler = FOURCC_NULL_CODEC;
                             pci->dwVersion = 0x00010000;
-                            pci->dwFlags = 0;
-                            _tcscpy_s(pci->szName, _countof(pci->szName), _T("Null Codec"));
-                            _tcscpy_s(pci->szDescription, _countof(pci->szDescription), _T("Pass-through VFW codec"));
+                            pci->dwFlags   = 0;
+                            _tcscpy_s(pci->szName,
+                                      _countof(pci->szName),
+                                      _T("Null Codec"));
+                            _tcscpy_s(pci->szDescription,
+                                      _countof(pci->szDescription),
+                                      _T("Pass-through VFW codec"));
                             res = ICERR_OK;
                         }
                     } break;
 
                     default:
                         res = LocalDefDriverProc(
-                            dwDriverID, hDriver, uMsg, lParam1, lParam2
+                            dwDriverID, hDriver,
+                            uMsg, lParam1, lParam2
                         );
                 }
             }
@@ -156,8 +168,7 @@ STDAPI DllUnregisterServer() { return S_OK; }
 // --- Sequence compression API implementation ---
 
 BOOL VFWAPI ICSeqCompressFrameStart(PCOMPVARS pc,
-                                    LPBITMAPINFO lpbiIn)
-{
+                                    LPBITMAPINFO lpbiIn) {
     InitSeqFuncs();
     // Open codec instance
     HIC hic = reinterpret_cast<HIC>(
@@ -169,7 +180,7 @@ BOOL VFWAPI ICSeqCompressFrameStart(PCOMPVARS pc,
 
     // Store handle and input info
     pc->lpState = reinterpret_cast<LPVOID>(hic);
-    pc->lpbiIn  = lpbiIn;
+    pc->lpbiIn = lpbiIn;
 
     // Begin compression: pass pointers to the header within BITMAPINFO
     LRESULT r = DriverProc(
@@ -185,14 +196,13 @@ LPVOID VFWAPI ICSeqCompressFrame(PCOMPVARS pc,
                                  UINT uiFlags,
                                  LPVOID lpBits,
                                  BOOL *pfKey,
-                                 LONG *plSize)
-{
+                                 LONG *plSize) {
     HIC hic = reinterpret_cast<HIC>(pc->lpState);
     ICCOMPRESS ic = {};
-    ic.dwFlags     = uiFlags;
-    ic.lpbiInput   = &pc->lpbiIn->bmiHeader;
-    ic.lpInput     = lpBits;
-    ic.lpdwFlags   = reinterpret_cast<LPDWORD>(pfKey);
+    ic.dwFlags = uiFlags;
+    ic.lpbiInput = &pc->lpbiIn->bmiHeader;
+    ic.lpInput = lpBits;
+    ic.lpdwFlags = reinterpret_cast<LPDWORD>(pfKey);
     ic.dwFrameSize = pc->lpbiIn->bmiHeader.biSizeImage;
 
     LRESULT sz = DriverProc(
@@ -207,8 +217,7 @@ LPVOID VFWAPI ICSeqCompressFrame(PCOMPVARS pc,
     return nullptr;
 }
 
-void VFWAPI ICSeqCompressFrameEnd(PCOMPVARS pc)
-{
+void VFWAPI ICSeqCompressFrameEnd(PCOMPVARS pc) {
     HIC hic = reinterpret_cast<HIC>(pc->lpState);
     DriverProc(
         reinterpret_cast<DWORD_PTR>(hic), nullptr,
